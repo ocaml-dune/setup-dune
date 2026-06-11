95f257e6314fde85b1a206b28470617adcd9f071
Samuel Hym <samuel@tarides.com>
2026-06-11 13:32:11 +0200

    WIP Retry curl 10 times

--- setupdune.sh
+++ setupdune.sh
@@ -58,9 +58,12 @@ install-dune() {
       explicit=y
       ;;
   esac
-  (set -x;
-    curl -fsSLv4 --http1.1 https://get.dune.build/install | \
-      sh -s ${explicit:+-- --release "$SETUPDUNEVERSION"}
+  (set -exo pipefail
+    for _ in 1 2 3 4 5 6 7 8 9 10; do
+      curl -fsSLv4 --http1.1 https://get.dune.build/install | \
+        sh -s ${explicit:+-- --release "$SETUPDUNEVERSION"} \
+        && break
+    done
     command -v dune
     dune --version)
   case "$(dune --version)" in
